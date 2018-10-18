4e5b1559898833650d93e68356d772c843ee75d2
DESKTOP-V1TKEKS\erkko <erkko.anttila@gmail.com>
2018-10-19 00:22:56 +0300

    commtting first C++ files

---
+++ BattleTank/Source/BattleTank/Private/TankPlayerController.cpp
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "Tank.h"
+#include "TankPlayerController.h"
+
+
+ATank* ATankPlayerController::GetControllerTank() const
+{
+	
+	return Cast<ATank>(GetPawn());
+
+}
+
